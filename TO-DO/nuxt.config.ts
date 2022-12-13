47a5eb66d679b7212b429ed426d4c19dc1d975fd
Prateek Aghi <prateekaghi42@gmail.com>
2022-12-13 14:54:01 +0530

    vuetify integration with nuxt 3

---
+++ TO-DO/nuxt.config.ts
@@ -0,0 +1,12 @@
+import vuetify from "./plugins/vuetify";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  css: [
+    "vuetify/lib/styles/main.sass",
+    "@mdi/font/css/materialdesignicons.min.css",
+  ],
+  build: {
+    transpile: ["vuetify"],
+  },
+});
