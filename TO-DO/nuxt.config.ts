200e34facb31152ebf80d4373612320e1303f008
Prateek Aghi <prateekaghi42@gmail.com>
2022-12-13 17:58:33 +0530

    bootstrap integrated

--- TO-DO/nuxt.config.ts
+++ TO-DO/nuxt.config.ts
@@ -2,11 +2,5 @@ import vuetify from "./plugins/vuetify";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: [
-    "vuetify/lib/styles/main.sass",
-    "@mdi/font/css/materialdesignicons.min.css",
-  ],
-  build: {
-    transpile: ["vuetify"],
-  },
+  css: ["bootstrap/dist/css/bootstrap.min.css"],
 });
